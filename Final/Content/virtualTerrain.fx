a1d1c003a37ffc2833f52dba5b36e8fd9583ac7d
content-deleted <contentdeleted@gmail.com>
2018-11-30 17:11:44 -0700

    feat: pulled in some code for perlin noise for terrain

---
+++ Final/Content/virtualTerrain.fx
@@ -0,0 +1,64 @@
+﻿#if OPENGL
+	#define SV_POSITION POSITION
+	#define VS_SHADERMODEL vs_3_0
+	#define PS_SHADERMODEL ps_3_0
+#else
+	#define VS_SHADERMODEL vs_4_0_level_9_1
+	#define PS_SHADERMODEL ps_4_0_level_9_1
+#endif
+
+float3 CameraPosition; // in world space
+float4x4 World; // World Matrix
+float4x4 View; // View Matrix
+float4x4 Projection; // Projection Matrix
+
+struct VertexShaderInput
+{
+    float4 Position : SV_Position0;
+   // float3 Normal : NORMAL0;
+    float2 UV : TEXCOORD0;
+    //float4 Color : COLOR0;
+    //float2 UV: TEXCOORD0;
+};
+
+struct VertexShaderOutput
+{
+	float4 Position : SV_POSITION;
+	//float4 Color : COLOR0;
+    float4 WorldPosition : TEXCOORD1;
+    //float3 WorldNormal : TEXCOORD2;
+};
+
+VertexShaderOutput MainVS(in VertexShaderInput input)
+{
+	VertexShaderOutput output = (VertexShaderOutput)0;
+    output.WorldPosition = mul(input.Position, World);
+    float4 viewPosition = mul(output.WorldPosition, View);
+    output.Position = mul(viewPosition, Projection);
+
+    //output.Color = input.Color; //olor : COLOR0;
+	// Send normal in world space
+   /// output.WorldNormal = mul(input.Normal, World);
+	
+
+	return output;
+}
+
+float4 MainPS(VertexShaderOutput input) : COLOR
+{
+    //bool edge = dot(CameraPosition, input.WorldNormal) == 0;
+    //float4 color = (1, 1, 1, ((int) edge));
+    float dist = 1 / (distance(CameraPosition, input.WorldPosition)/15);
+
+    return float4(1, 0, 0, dist*dist); //input.Color;
+
+}
+
+technique BasicColorDrawing
+{
+	pass P0
+	{
+		VertexShader = compile VS_SHADERMODEL MainVS();
+		PixelShader = compile PS_SHADERMODEL MainPS();
+	}
+};
--- Game Engine/Material.cs
+++ Game Engine/Material.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -157,6 +158,95 @@ namespace CPI311.GameEngine {
             effect.Parameters["DiffuseColor"].SetValue(diffuseColor);
             effect.Parameters["SpecularColor"].SetValue(specularColor);
             effect.Parameters["NormalMap"].SetValue(NormalMap);
+
+            foreach (EffectPass pass in effect.CurrentTechnique.Passes) {
+                pass.Apply();
+                g.DrawUserIndexedPrimitives<VertexPositionTexture>
+                (PrimitiveType.TriangleList, Vertices, 0, Vertices.Length, Indices, 0, Indices.Length / 3);
+            }
+        }
+
+    }
+
+    public class CustomTerrainRenderer : Material {
+        public static Effect effect;
+
+        
+        private VertexPositionTexture[] Vertices { get; set; }
+
+        private int[] Indices { get; set; }
+        private float[] heights { get; set; }
+
+        private int rows, cols;
+
+        private Perlin noise = new Perlin();
+
+        public CustomTerrainRenderer(Vector2 res) {
+
+            rows = (int)res.Y + 1;
+            cols = (int)res.X + 1;
+
+            heights = new float[rows * cols];
+            //CreateHeights(); 
+
+            Vertices = new VertexPositionTexture[rows * cols];
+
+            for (int r = 0; r < rows; r++)
+                for (int c = 0; c < cols; c++)
+                    Vertices[r * cols + c] = new VertexPositionTexture(
+                        new Vector3(c, GetHeight( (10f*c/(float)rows) , (10f*r/(float)cols) ), r),
+                        new Vector2(c / res.X, r / res.Y));
+
+            Indices = new int[(rows - 1) * (cols - 1) * 6];
+            int index = 0;
+            for (int r = 0; r < rows - 1; r++)
+                for (int c = 0; c < cols - 1; c++) {
+                    Indices[index++] = r * cols + c;
+                    Indices[index++] = r * cols + c + 1;
+                    Indices[index++] = (r + 1) * cols + c;
+
+                    Indices[index++] = (r + 1) * cols + c;
+                    Indices[index++] = r * cols + c + 1;
+                    Indices[index++] = (r + 1) * cols + c + 1;
+                }
+        }
+        public float GetHeight(double x, double y) {
+            return (float)noise.OctavePerlin(x, y, 0.5f, 2, 10) *10;
+        }
+        
+       
+
+        public float GetAltitude(Vector3 position) {
+            /*position = Vector3.Transform(position, Matrix.Invert(ourObject.transform.World));
+            if (position.X > -size.X / 2 && position.X < size.X / 2 &&
+                              position.Z > -size.Y / 2 && position.Z < size.Y / 2)
+                return GetHeight(new Vector2((position.X + size.X / 2) / size.X,
+                    (position.Z + size.Y / 2) / size.Y)) * ourObject.transform.LocalScale.Y;*/
+            return -1;
+        }
+
+        public Vector3 lightPosition;
+        float shininess = 0.3f;
+
+        public Vector3 ambientColor = new Vector3(0.3f, 0.2f, 0.2f);
+        public Vector3 diffuseColor = new Vector3(0.1f, 0.1f, 0.1f);
+        public Vector3 specularColor = new Vector3(0.2f, 0.2f, 0.2f);
+
+        public override void Render(Camera c, Transform t, Model m, GraphicsDevice g) {
+            Matrix view = c.View;
+            Matrix projection = c.Projection;
+
+            // Setup custom shader etc.
+            effect.CurrentTechnique = effect.Techniques[0];
+            effect.Parameters["World"].SetValue(t.World);
+            effect.Parameters["View"].SetValue(view);
+            effect.Parameters["Projection"].SetValue(projection);
+            //effect.Parameters["LightPosition"].SetValue(lightPosition);
+            effect.Parameters["CameraPosition"].SetValue(c.Transform.Position);
+            /*effect.Parameters["Shininess"].SetValue(shininess);
+            effect.Parameters["AmbientColor"].SetValue(ambientColor);
+            effect.Parameters["DiffuseColor"].SetValue(diffuseColor);
+            effect.Parameters["SpecularColor"].SetValue(specularColor);*/
 
             foreach (EffectPass pass in effect.CurrentTechnique.Passes) {
                 pass.Apply();
