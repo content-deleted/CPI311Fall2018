8aec545812bc97d55cf73a2c9acb65e357ebb142
content-deleted <contentdeleted@gmail.com>
2018-12-04 02:53:29 -0700

    feat: added content for a simple enemy

---
+++ Assignment1/BasicShootEnemy.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+
+using CPI311.GameEngine;
+using System.Diagnostics;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Assignment1 {
+    class BasicShootEnemy : Behavior2d {
+
+        private enemyHealth health;
+        private BulletSpawner spawner;
+        private Random ran;
+
+        public Texture2D bulletSprite;
+
+        int state = 0;
+
+        public override void Start() {
+            base.Start();
+            ran = new Random();
+            health = obj.GetBehavior<enemyHealth>() as enemyHealth;
+            spawner = obj.GetBehavior<BulletSpawner>() as BulletSpawner;
+            spawner.scale = Vector2.One * 0.5f;
+            spawner.bulletSpeed = 2;
+            spawner.bulletSprite = bulletSprite;
+        }
+        float speed = 4;
+
+
+        Vector2 direction;
+        int timer= 10;
+        public override void Update() {
+            base.Update();
+            timer--;
+            if (timer <= 0) {
+                timer = ran.Next(10, 40);
+                direction = new Vector2((float)ran.NextDouble(), (float)ran.NextDouble());
+            }
+
+            objSprite.move(  direction * speed );
+        }
+    }
+}
---
+++ Assignment1/BasicTargetEnemy.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+
+using CPI311.GameEngine;
+using System.Diagnostics;
+
+namespace Assignment1 {
+    class BossController : Behavior2d {
+        public ProgressBar healthbar;
+
+        private enemyHealth health;
+        private BulletSpawner spawner;
+
+        int state = 0;
+
+        public override void Start() {
+            base.Start();
+            health = obj.GetBehavior<enemyHealth>() as enemyHealth;
+            health.boss = true;
+            spawner = obj.GetBehavior<BulletSpawner>() as BulletSpawner;
+        }
+
+        public override void Update() {
+            base.Update();
+            updateHealthbar();
+
+            if(state == 0) spawner.scale = Vector2.One * (float) (Math.Abs(Math.Sin(Time.TotalGameTimeMilli * 100))* .75 + 0.33) ;
+
+            if (state == 2) {
+                spawner.bulletSpeed = (float)Math.Sin(Time.TotalGameTimeMilli * 100) * 5 ;
+            }
+        }
+
+        public void updateHealthbar() => healthbar.value = health.health / health.max;
+
+        public void changeState() {
+            state++;
+
+            updateHealthbar();
+
+            switch (state) {
+                case (1):
+                    health.health = health.max;
+                    healthbar.innerSprite.Color = Color.ForestGreen;
+                    // spawn pattern
+
+                    spawner.bulletSpeed = 4;
+                    spawner.bulletAmount = 6;
+
+                    spawner.bulletfrequency = 0.05f; // ms 
+
+                    spawner.spin = 1.8f;
+                    spawner.Wave = 0;
+
+                    spawner.bulletTint = Color.ForestGreen;
+                    //spawner.bulletSprite = bulletSprite;
+                    spawner.scale = Vector2.One * .5f;
+
+                    break;
+
+                case (2):
+                    health.health = health.max;
+                    healthbar.innerSprite.Color = Color.DarkBlue;
+
+                    spawner.bulletAmount = 15;
+                    spawner.spin = 0.4f;
+                    spawner.scale = Vector2.One * 0.4f;
+                    spawner.bulletTint = Color.DarkBlue;
+                    break;
+
+                case (3):
+                    enemyHealth.enemies.Remove(health);
+                    (obj as GameObject2d).Destroy();
+                    break;
+
+            }
+
+        }
+    }
+}
